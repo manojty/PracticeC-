8daa0a2eb4a967b40b5de2944bd72b097bcaad87
matyagi <matyagi@gmail.com>
2022-12-28 17:33:42 -0800

    Bubble sort

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace PracticeC_
     {
         static void Main(string[] args)
         {
-            TestSelectionSort.runAllTests();
+            TestBubbleSort.runAllTests();
         }
     }
 }
